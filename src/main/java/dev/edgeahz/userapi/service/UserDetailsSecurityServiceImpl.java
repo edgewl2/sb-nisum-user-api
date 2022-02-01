e043c62f08d27f4354815830c75d04df55e3884d
Gerardo <edgewl2@gmail.com>
2022-02-01 06:05:53 -0600

    feat: user datails security service implementation
    This is a service used mainly by spring security. For this project it's used to interact with
    authentication and JWT allows Sign in without credentials.

---
+++ src/main/java/dev/edgeahz/userapi/service/UserDetailsSecurityServiceImpl.java
@@ -0,0 +1,60 @@
+package dev.edgeahz.userapi.service;
+
+import dev.edgeahz.userapi.domain.entity.User;
+import dev.edgeahz.userapi.repository.UserRepository;
+import dev.edgeahz.userapi.security.provider.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+@Service
+public class UserDetailsSecurityServiceImpl implements UserDetailsSecurityService {
+
+    private final UserRepository userRepository;
+
+    private final JwtProvider jwtProvider;
+
+    @Autowired
+    public UserDetailsSecurityServiceImpl(UserRepository userRepository, JwtProvider jwtProvider) {
+        this.userRepository = userRepository;
+        this.jwtProvider = jwtProvider;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() ->
+                new UsernameNotFoundException("email.not.found"));
+
+        return withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRoles())
+                .credentialsExpired(false)
+                .accountLocked(false)
+                .accountExpired(false)
+                .disabled(false)
+                .build();
+    }
+
+    @Override
+    public Optional<UserDetails> loadUserByJwtToken(String token) {
+
+        if (jwtProvider.validateToken(token)) {
+            return Optional.of(
+                    withUsername(jwtProvider.getUsername(token))
+                            .authorities(jwtProvider.getRoles(token))
+                            .password("")
+                            .accountExpired(false)
+                            .accountLocked(false)
+                            .credentialsExpired(false)
+                            .disabled(false)
+                            .build());
+        }
+        return Optional.empty();
+    }
+}
